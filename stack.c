58b17a05d07b5f9f37f8a02c1510bc3d8b8d939b
Emma Christy <emma.t.christy@gmail.com>
2024-04-29 17:31:15 +0300

    added 0 and overflow error conditions

--- stack.c
+++ stack.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 
-stack_init(size_t type_size, size_t element_number)
+void *stack_init(size_t type_size, size_t element_number)
 {
+	size_t alloc_size = type_size * element_number;
+
+	//returns null if no data to be allocated
+	if(alloc_size == 0)
+		return NULL;
+
+	//returns null if multiplication overflows
+	if(alloc_size / element_number != type_size)
+		return NULL;
+
 
 }
