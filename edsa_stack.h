1e75b1254177eefedc20b7a40d93e08f6e7de0d4
Emma Christy <emma.t.christy@gmail.com>
2024-05-19 14:14:11 +0300

    renamed edsa_stack_size to edsa_stack_place

--- edsa_stack.h
+++ edsa_stack.h
@@ -15,6 +15,6 @@ edsa_stack *edsa_stack_init(size_t type_size, size_t element_number);
 
 void edsa_stack_free(edsa_stack *stack);
 
-size_t edsa_stack_size(edsa_stack *stack);
+size_t edsa_stack_place(edsa_stack *stack);
 
 #endif
