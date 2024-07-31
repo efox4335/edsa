1e1d741328731c1df1c89d408f4b56879cbc14aa
Emma Christy <emma.t.christy@gmail.com>
2024-07-31 08:52:28 +0300

    added basic push pop test

---
+++ tests/edsa_stack_tests/edsa_stack_push_pop.c
@@ -0,0 +1,23 @@
+#include "../../src/edsa_stack/edsa_stack.h"
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void)
+{
+	edsa_stack *stack;
+
+	srand(time(NULL));
+
+	int element = rand();
+
+	edsa_stack_init(sizeof(int), 1, &stack);
+
+	edsa_stack_push(stack, &element);
+
+	int check;
+
+	edsa_stack_pop(stack, &check);
+
+	return !(check == element);
+}
