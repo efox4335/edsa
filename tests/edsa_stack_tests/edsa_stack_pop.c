28974b909441858e76e58c6b445c8d206eb38575
Emma Christy <emma.t.christy@gmail.com>
2024-08-01 15:07:34 +0300

    added stack pop test

---
+++ tests/edsa_stack_tests/edsa_stack_pop.c
@@ -0,0 +1,18 @@
+#include "../../src/edsa.h"
+
+int main(void)
+{
+	edsa_stack *stack;
+
+	int place_holder = 3;
+
+	edsa_stack_init(sizeof(int), 1, &stack);
+
+	edsa_stack_push(stack, &place_holder);
+
+	int ret_value = edsa_stack_pop(stack, &place_holder);
+
+	edsa_stack_free(stack);
+
+	return !(ret_value == EDSA_SUCCESS);
+}
