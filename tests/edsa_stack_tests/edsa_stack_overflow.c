08b32f260840d3f6c427c0c4b3304036c4167ebe
Emma Christy <emma.t.christy@gmail.com>
2024-08-23 10:21:48 +0300

    added error reporting to edsa_stack_overflow.c

--- tests/edsa_stack_tests/edsa_stack_overflow.c
+++ tests/edsa_stack_tests/edsa_stack_overflow.c
@@ -1,8 +1,11 @@
 #include "../../src/edsa.h"
+#include <stdio.h>
 
 int main(void)
 {
 	edsa_stack *stack;
+	char *fail_err = NULL;
+	char *ret_str = NULL;
 
 	int place_holder = 3;
 
@@ -12,7 +15,15 @@ int main(void)
 
 	int ret_value = edsa_stack_push(stack, &place_holder);
 
+	if(ret_value != EDSA_STACK_PUSH_STACK_FULL){
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_STACK_PUSH_STACK_FULL, &ret_str);
+		printf("edsa_stack_overflow.c: edsa_stack_push failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		return 1;
+	}
+
 	edsa_stack_free(stack);
 
-	return !(ret_value == EDSA_STACK_PUSH_STACK_FULL);
+	return 0;
 }
