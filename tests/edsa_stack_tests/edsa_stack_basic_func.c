70cd3572c2c74cd68753386cc8a340c5376b7c2c
Emma Christy <emma.t.christy@gmail.com>
2024-08-23 10:11:18 +0300

    added edsa_stack_basic_func.c

--- tests/edsa_stack_tests/edsa_stack_basic_func.c
+++ tests/edsa_stack_tests/edsa_stack_basic_func.c
@@ -4,24 +4,73 @@
 int main(void)
 {
 	edsa_stack *stack;
-	int place_holder = 0xaaaaaaa;
+	size_t place_holder = 0xaaaaaaa;
 	int ret_value;
+	char *fail_err = NULL;
+	char *ret_str = NULL;
 
-	ret_value = edsa_stack_init(sizeof(int), 1, &stack);
+	ret_value = edsa_stack_init(sizeof(size_t), 1, &stack);
 	if(ret_value != EDSA_SUCCESS){
-		edsa_print_error(stdout, ret_value);
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_stack_init failed to return %s instead returned %s\n",
+		ret_str, fail_err);
 		return 1;
 	}
 
 	ret_value = edsa_stack_push(stack, &place_holder);
 	if(ret_value != EDSA_SUCCESS){
-		edsa_print_error(stdout, ret_value);
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_stack_push failed to return %s instead returned %s\n",
+		ret_str, fail_err);
 		return 1;
 	}
 
-	ret_value = edsa_stack
+	ret_value = edsa_stack_pop(stack, &place_holder);
+	if(ret_value != EDSA_SUCCESS){
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_stack_pop failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		return 1;
+	}
 
-	edsa_stack_free(stack);
+	ret_value = edsa_stack_available_elements(stack, &place_holder);
+	if(ret_value != EDSA_SUCCESS){
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_stack_available_elements failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		return 1;
+	}
 
-	return !(ret_value == EDSA_SUCCESS);
+	ret_value = edsa_stack_size(stack, &place_holder);
+	if(ret_value != EDSA_SUCCESS){
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_stack_size failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		return 1;
+	}
+
+	ret_value = edsa_stack_place(stack, &place_holder);
+	if(ret_value != EDSA_SUCCESS){
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_stack_place failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		return 1;
+	}
+
+	ret_value = edsa_stack_free(stack);
+	if(ret_value != EDSA_SUCCESS){
+		edsa_get_error_string(ret_value, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_stack_free failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		return 1;
+	}
+
+	return 0;
 }
