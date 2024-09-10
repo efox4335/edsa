b64d7271a4c875c24803e5e7f6357d1357d0a7ef
Emma Christy <emma.t.christy@gmail.com>
2024-09-10 18:45:25 +0300

    removed extra newlines form edsa_get_error_string

--- src/edsa_error_codes/edsa_error_codes.c
+++ src/edsa_error_codes/edsa_error_codes.c
@@ -6,31 +6,31 @@ size_t edsa_get_error_string(size_t error, char **str)
 {
 	switch(error){
 		case EDSA_SUCCESS:
-			*str = "EDSA_SUCCESS\n";
+			*str = "EDSA_SUCCESS";
 			break;
 		case EDSA_STACK_INIT_STACK_MALLOC_FAILED:
-			*str = "EDSA_STACK_INIT_STACK_MALLOC_FAILED\n";
+			*str = "EDSA_STACK_INIT_STACK_MALLOC_FAILED";
 			break;
 		case EDSA_STACK_INIT_STACK_METADATA_MALLOC_FAILED:
-			*str = "EDSA_STACK_INIT_STACK_METADATA_MALLOC_FAILED\n";
+			*str = "EDSA_STACK_INIT_STACK_METADATA_MALLOC_FAILED";
 			break;
 		case EDSA_STACK_INIT_STACK_SIZE_MULTIPLICATION_OVERFLOW:
-			*str = "EDSA_STACK_INIT_STACK_SIZE_MULTIPLICATION_OVERFLOW\n";
+			*str = "EDSA_STACK_INIT_STACK_SIZE_MULTIPLICATION_OVERFLOW";
 			break;
 		case EDSA_STACK_INIT_ZERO_ALLOC_SIZE:
-			*str = "EDSA_STACK_INIT_ZERO_ALLOC_SIZE\n";
+			*str = "EDSA_STACK_INIT_ZERO_ALLOC_SIZE";
 			break;
 		case EDSA_STACK_POP_STACK_EMPTY:
-			*str = "EDSA_STACK_POP_STACK_EMPTY\n";
+			*str = "EDSA_STACK_POP_STACK_EMPTY";
 			break;
 		case EDSA_STACK_PUSH_STACK_FULL:
-			*str = "EDSA_STACK_PUSH_STACK_FULL\n";
+			*str = "EDSA_STACK_PUSH_STACK_FULL";
 			break;
 		case EDSA_STACK_REALLOC_FAILED:
 			*str = "EDSA_STACK_REALLOC_FAILED";
 			break;
 		default:
-			*str = "UNKNOWN_ERROR\n";
+			*str = "UNKNOWN_ERROR";
 			break;
 
 	}
