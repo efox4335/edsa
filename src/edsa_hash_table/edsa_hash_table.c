0df94921c1e823ec43ee609baffd7460ff3c4ca5
Emma Christy <emma.t.christy@gmail.com>
2024-09-23 14:45:53 +0300

    changed hash function to support size_t

--- src/edsa_hash_table/edsa_hash_table.c
+++ src/edsa_hash_table/edsa_hash_table.c
@@ -1,9 +1,11 @@
-//fnv-1a hash
-static unsigned long hash(unsigned char *restrict key, const int len)
-{
-	unsigned long hash = 14695981039346656037LU;
+#include <stdlib.h>
 
-	for(int i = 0; i < len; ++i){
+//fnv-1a hash
+static size_t hash(unsigned char *restrict key, const size_t len)
+{
+	size_t hash = 14695981039346656037LU;
+
+	for(size_t i = 0; i < len; ++i){
 		hash ^= key[i];
 		hash *= 1099511628211;
 	}
