f89a232aac713d1d6885b395b35b63cdaaf37e51
Emma Christy <emma.t.christy@gmail.com>
2024-09-23 16:12:27 +0300

    added next_largest_prime

--- src/edsa_table/edsa_table.c
+++ src/edsa_table/edsa_table.c
@@ -13,3 +13,27 @@ static size_t hash(unsigned char *restrict key, const size_t len)
 
 	return hash;
 }
+
+//todo add arr of large primes and improve alg
+static size_t next_largest_prime(size_t num)
+{
+	size_t pos_prime = num;
+	size_t factor = 0;
+	size_t quotient = 0;
+
+	while(1){
+		factor = 1;
+		quotient = 3;
+
+		while((quotient > factor) && (pos_prime != quotient * factor)){
+			++factor;
+			quotient = pos_prime / factor;
+		}
+
+		if(pos_prime != quotient * factor){
+			return pos_prime;
+		}
+
+		++pos_prime;
+	}
+}
