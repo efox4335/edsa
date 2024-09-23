2ec8787bd50c2bbfb2829f1107effda9b97cc4f1
Emma Christy <emma.t.christy@gmail.com>
2024-09-23 14:42:51 +0300

    fixed hash function

--- src/edsa_hash_table/edsa_hash_table.c
+++ src/edsa_hash_table/edsa_hash_table.c
@@ -1,5 +1,5 @@
 //fnv-1a hash
-unsigned long hash(unsigned char *key, int len)
+static unsigned long hash(unsigned char *restrict key, const int len)
 {
 	unsigned long hash = 14695981039346656037LU;
 
