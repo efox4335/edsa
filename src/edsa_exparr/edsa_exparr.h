1a2e3ec2d1b84b9f1c808b327c579abfd90304b3
Emma Christy <emma.t.christy@gmail.com>
2024-10-05 09:39:54 +0300

    added edsa_exparr data type and func defs

---
+++ src/edsa_exparr/edsa_exparr.h
@@ -0,0 +1,16 @@
+#ifndef __edsa_exparr__
+#define __edsa_exparr__
+
+#include <stddef.h>
+
+typedef struct{
+	size_t arr_size;
+	size_t data_size;
+	void *restrict arr;
+}edsa_exparr;
+
+size_t edsa_exparr_init(edsa_exparr *restrict *const restrict arr, const size_t arr_size, const size_t data_size);
+size_t edsa_exparr_free(edsa_exparr *const restrict arr);
+size_t edsa_exparr_ins(edsa_exparr *const restrict arr, size_t index, void *const restrict data);
+
+#endif
