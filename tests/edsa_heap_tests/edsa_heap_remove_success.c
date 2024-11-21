185512779eb5981c7e84b6c921538675c07b8784
Emma Christy <emma.t.christy@gmail.com>
2024-11-21 11:35:16 +0200

    fixed typo

--- tests/edsa_heap_tests/edsa_heap_remove_success.c
+++ tests/edsa_heap_tests/edsa_heap_remove_success.c
@@ -21,7 +21,7 @@ int main(void)
 	if(ret_val != EDSA_SUCCESS){
 		edsa_get_error_string(ret_val, &fail_err);
 		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
-		printf("edsa_heap_remove_output.c: edsa_heap_remove failed to return %s instead returned %s\n",
+		printf("edsa_heap_remove_success.c: edsa_heap_remove failed to return %s instead returned %s\n",
 		ret_str, fail_err);
 		fflush(stdout);
 		edsa_heap_free(heap);
