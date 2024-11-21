8f53017a772161a626b839587402ebb17fed769c
Emma Christy <emma.t.christy@gmail.com>
2024-11-21 13:17:32 +0200

    added random input data test

--- tests/edsa_heap_tests/edsa_heap_remove_output.c
+++ tests/edsa_heap_tests/edsa_heap_remove_output.c
@@ -1,5 +1,7 @@
 #include "edsa.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int cmp_func(const void *const a, const void *const b)
 {
@@ -13,12 +15,52 @@ void set_arr(size_t *arr, size_t size)
 	}
 }
 
+//fisher yates
+void scramble(size_t input[], size_t size)
+{
+	size_t swap_spot;
+	size_t temp;
+
+	srand(time(NULL));
+
+	for(size_t i = 0; i < size - 1; ++i){
+		swap_spot = (rand() % (size - i)) + i;
+
+		temp = input[i];
+		input[i] = input[swap_spot];
+		input[swap_spot] = temp;
+	}
+}
+
 //returns index of failed data
 //returns size on pass
 size_t check_arr(size_t *arr, size_t size)
 {
 	for(size_t i = 0; i < size; ++i){
 		if(arr[i] != i){
+			return i;
+		}
+	}
+
+	return size;
+}
+
+//sets each element to an integer between size - 1 and 0
+void rand_set_arr(size_t *arr, size_t size)
+{
+	srand(time(NULL));
+
+	for(size_t i = 0; i < size; ++i){
+		arr[i] = rand() % (size - 1);
+	}
+}
+
+//returns second index of first unsorted pair
+//returns size on pass
+size_t check_sorted(size_t *arr, size_t size)
+{
+	for(size_t i = 1; i < size; ++i){
+		if(arr[i - 1] > arr[i]){
 			return i;
 		}
 	}
@@ -51,9 +93,63 @@ int main(void)
 	ret_val = check_arr(data, DATA_AMOUNT);
 
 	if(ret_val != DATA_AMOUNT){
-		printf("data returned at index %ld not expected expected %ld instead got %ld\n",\
+		printf("sorted data returned at index %ld not expected expected %ld instead got %ld\n",
 		ret_val,
 		ret_val,
+		data[ret_val]);
+		edsa_heap_free(heap);
+		return 1;
+	}
+
+	edsa_heap_free(heap);
+
+
+
+	edsa_heap_init(&heap, 1, sizeof (size_t), cmp_func);
+
+	scramble(data, DATA_AMOUNT);
+
+	for(int i = 0; i < DATA_AMOUNT; ++i){
+		edsa_heap_ins(heap, &(data[i]));
+	}
+
+	for(int i = 0; i < DATA_AMOUNT; ++i){
+		edsa_heap_remove(heap, &(data[i]));
+	}
+
+	ret_val = check_arr(data, DATA_AMOUNT);
+
+	if(ret_val != DATA_AMOUNT){
+		printf("scrambled data returned at index %ld not expected expected %ld instead got %ld\n",
+		ret_val,
+		ret_val,
+		data[ret_val]);
+		edsa_heap_free(heap);
+		return 1;
+	}
+
+	edsa_heap_free(heap);
+
+
+
+	edsa_heap_init(&heap, 1, sizeof (size_t), cmp_func);
+
+	rand_set_arr(data, DATA_AMOUNT);
+
+	for(int i = 0; i < DATA_AMOUNT; ++i){
+		edsa_heap_ins(heap, &(data[i]));
+	}
+
+	for(int i = 0; i < DATA_AMOUNT; ++i){
+		edsa_heap_remove(heap, &(data[i]));
+	}
+
+	ret_val = check_sorted(data, DATA_AMOUNT);
+
+	if(ret_val != DATA_AMOUNT){
+		printf("randomized data returned at index %ld not sorted got values %d and %d\n",
+		ret_val,
+		data[ret_val - 1],
 		data[ret_val]);
 		edsa_heap_free(heap);
 		return 1;
