1da4915656bd575db25dad4c435ba15d2f6e6839
Emma Christy <emma.t.christy@gmail.com>
2024-11-21 12:54:00 +0200

    fixed typo

--- tests/edsa_heap_tests/edsa_heap_remove_output.c
+++ tests/edsa_heap_tests/edsa_heap_remove_output.c
@@ -28,29 +28,29 @@ size_t check_arr(size_t *arr, size_t size)
 
 int main(void)
 {
-	enum{DATA_AMMOUNT = 10};
+	enum{DATA_AMOUNT = 1000};
 
 	size_t ret_val = 0;
 	char *fail_err = NULL;
 	char *ret_str = NULL;
 	edsa_heap *heap;
-	size_t data[DATA_AMMOUNT];
+	size_t data[DATA_AMOUNT];
 
 	edsa_heap_init(&heap, 1, sizeof (size_t), cmp_func);
 
-	set_arr(data, DATA_AMMOUNT);
+	set_arr(data, DATA_AMOUNT);
 
-	for(int i = 0; i < DATA_AMMOUNT; ++i){
+	for(int i = 0; i < DATA_AMOUNT; ++i){
 		edsa_heap_ins(heap, &(data[i]));
 	}
 
-	for(int i = 0; i < DATA_AMMOUNT; ++i){
+	for(int i = 0; i < DATA_AMOUNT; ++i){
 		edsa_heap_remove(heap, &(data[i]));
 	}
 
-	ret_val = check_arr(data, DATA_AMMOUNT);
+	ret_val = check_arr(data, DATA_AMOUNT);
 
-	if(ret_val != DATA_AMMOUNT){
+	if(ret_val != DATA_AMOUNT){
 		printf("data returned at index %ld not expected expected %ld instead got %ld\n",\
 		ret_val,
 		ret_val,
