ee1ac9e3b20a1e0ec68089484b24f88b45211bd8
Emma Christy <emma.t.christy@gmail.com>
2024-11-25 11:54:15 +0200

    added test to check the return value of edsa_htable_init() on successful call

---
+++ tests/edsa_htable_tests/edsa_htable_init_success.c
@@ -0,0 +1,26 @@
+#include "edsa.h"
+#include <stdio.h>
+
+int main(void)
+{
+	edsa_htable *htable;
+	size_t ret_val = 0;
+	char *fail_err = NULL;
+	char *ret_str = NULL;
+
+	edsa_htable_init(&htable, sizeof(int), sizeof(int), 1);
+
+	if(ret_val != EDSA_SUCCESS){
+		edsa_get_error_string(ret_val, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_htable_init_success.c: edsa_htable_init failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		fflush(stdout);
+		edsa_htable_free(htable);
+		return 1;
+	}
+
+	edsa_htable_free(htable);
+
+	return 0;
+}
