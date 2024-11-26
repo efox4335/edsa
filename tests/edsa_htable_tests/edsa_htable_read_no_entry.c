aa8e570fa2c3ffb10d7e4b2679a6cf9abb2f0bfb
Emma Christy <emma.t.christy@gmail.com>
2024-11-26 11:53:57 +0200

    added test the check the return value of edsa_htable_read() when there is no entry with the key passed

---
+++ tests/edsa_htable_tests/edsa_htable_read_no_entry.c
@@ -0,0 +1,29 @@
+#include "edsa.h"
+#include <stdio.h>
+
+int main(void)
+{
+	edsa_htable *htable;
+	size_t ret_val = 0;
+	char *fail_err = NULL;
+	char *ret_str = NULL;
+	int key = 1;
+	int data = 1;
+
+	edsa_htable_init(&htable, sizeof(int), sizeof(int), 1);
+	ret_val = edsa_htable_read(htable, &key, &data);
+
+	if(ret_val != EDSA_HTABLE_READ_NO_ENTRY){
+		edsa_get_error_string(ret_val, &fail_err);
+		edsa_get_error_string(EDSA_HTABLE_READ_NO_ENTRY, &ret_str);
+		printf("edsa_htable_read_no_entry.c: edsa_htable_read failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		fflush(stdout);
+		edsa_htable_free(htable);
+		return 1;
+	}
+
+	edsa_htable_free(htable);
+
+	return 0;
+}
