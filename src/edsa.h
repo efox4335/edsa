24e0722e03ecff28eada5467613e9518aff7c780
Emma Christy <emma.t.christy@gmail.com>
2024-11-29 11:40:00 +0200

    added target_include_directories for all headers to avoid paths in header files

--- src/edsa.h
+++ src/edsa.h
@@ -1,10 +1,10 @@
 #ifndef __edsa__
 #define __edsa__
 
-#include "edsa_error_codes/edsa_error_codes.h"
-#include "edsa_stack/edsa_stack.h"
-#include "edsa_exparr/edsa_exparr.h"
-#include "edsa_heap/edsa_heap.h"
-#include "edsa_htable/edsa_htable.h"
+#include "edsa_error_codes.h"
+#include "edsa_stack.h"
+#include "edsa_exparr.h"
+#include "edsa_heap.h"
+#include "edsa_htable.h"
 
 #endif
