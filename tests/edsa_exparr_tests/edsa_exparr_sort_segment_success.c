ca04811143cac33d8e0385dc869e7249967df2e6
Emma Christy <emma.t.christy@gmail.com>
2024-12-01 10:32:42 +0200

    added test to check the return value of edsa_exparr_sort_segment() on successful run

---
+++ tests/edsa_exparr_tests/edsa_exparr_sort_segment_success.c
@@ -0,0 +1,37 @@
+#include "edsa.h"
+#include <stdio.h>
+
+int cmp_func(const void *ele_1, const void *ele_2)
+{
+	return *((int *)ele_1) > *((int *) ele_2);
+}
+
+int main(void)
+{
+	edsa_exparr *arr;
+	size_t ele_num = 10;
+	size_t ret_val = 0;
+	int temp[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	char *fail_err = NULL;
+	char *ret_str = NULL;
+
+	edsa_exparr_init(&arr, ele_num, sizeof(int));
+	for(int i = 0; i < 10; ++i){
+		edsa_exparr_ins(arr, i, &(temp[i]));
+	}
+	ret_val = edsa_exparr_sort_segment(arr, cmp_func, 0, 9);
+
+	if(ret_val != EDSA_SUCCESS){
+		edsa_get_error_string(ret_val, &fail_err);
+		edsa_get_error_string(EDSA_SUCCESS, &ret_str);
+		printf("edsa_exparr_sort_segment_success.c: edsa_exparr_sort_segment failed to return %s instead returned %s\n",
+		ret_str, fail_err);
+		fflush(stdout);
+		edsa_exparr_free(arr);
+		return 1;
+	}
+
+	edsa_exparr_free(arr);
+
+	return 0;
+}
